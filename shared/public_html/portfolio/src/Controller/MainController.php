8ed992e2c213662601cecdc94c7e9109e604319b
Dimitri MARTEL <dimitri.martel@inrae.fr>
2024-10-01 15:29:50 +0200

    Création du jeu de la vie

---
+++ shared/public_html/portfolio/src/Controller/MainController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function homePage(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/projets', name: 'projets')]
+    public function projetsPage(): Response
+    {
+        return $this->render('projets/projets.html.twig');
+    }
+
+    #[Route('/experience/jeuDeLaVie', name: 'jeuDeLaVie')]
+    public function jeuDeLaVie(): Response
+    {
+        return $this->render('experience/jeuDeLaVie.html.twig');
+    }
+}
